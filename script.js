452ab197df1131107ba8eca3bf5727a9b2be3f39
Neil Shankar <7357971+tallneil@users.noreply.github.com>
2021-09-24 20:27:28 +0000

    everything works. going to try to make snap work now

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var gui;
 var obj = {
-  radius: 20,
+  radius: 30,
   opacity: 0.6,
-  magnet: 0.2,
+  magnet: 0.5,
 };
 var cursor;
 var cursor_r = obj.radius;
@@ -12,7 +12,7 @@ function makeControls() {
   gui = new dat.gui.GUI();
   gui.remember(obj);
   
-  gui.add(obj, 'radius').min(1).max(80).step(1).onChange(newValue => {cursor_r = newValue});
+  gui.add(obj, 'radius').min(10).max(120).step(1).onChange(newValue => {cursor_r = newValue});
   gui.add(obj, 'opacity').min(0).max(1).step(0.01).onChange(newValue => {cursor_a = newValue});
   gui.add(obj, 'magnet').min(0).max(1).step(0.01).onChange(newValue => {changeIntensity(0, newValue)});
 }
@@ -25,11 +25,11 @@ function handleCursor() {
   var buttons = document.getElementsByClassName("magnet");
   for (let b of buttons) {
     b.onmousemove = function(e) {
-      var xDiff = Math.floor( (e.offsetX - (this.offsetWidth / 2)) * obj.magnet); 
-      var yDiff = Math.floor( (e.offsetY - (this.offsetHeight / 2)) * obj.magnet);
+      var xDiff = Math.floor((e.offsetX - (this.offsetWidth / 2)) * obj.magnet * 0.6); 
+      var yDiff = Math.floor((e.offsetY - (this.offsetHeight / 2)) * obj.magnet * 0.6);
       b.style.transform = "translate(" + xDiff + "px, " + yDiff + "px)"; 
       cursor_r = obj.radius * 2;
-      cursor_a = obj.opacity * 2;
+      cursor_a = 1;
     }
     b.onmouseout = function(e) {
       b.style.transform = ""; 
@@ -39,7 +39,6 @@ function handleCursor() {
   }
 }
 
-// update cursor on mouse move
 document.addEventListener("mousemove", function(e) {
   var cursor_x = e.pageX - cursor_r;
   var cursor_y = e.pageY - cursor_r;
